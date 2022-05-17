3c39f932994d1da94251853c158e1bd1bd58b87f
ibenitosoto <ignaciobenitosoto@gmail.com>
2022-05-17 15:53:54 +0100

    Added date picker and dropdown events and logic to handle wrong inputs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,8 +23,7 @@ namespace CA3_s00220273
     {
         GuitarsAndBookingsEntities db = new GuitarsAndBookingsEntities();
 
-        ObservableCollection<Guitar> dropdownColorsList = new ObservableCollection<Guitar>();
-        ObservableCollection<Guitar> availableGuitarsList = new ObservableCollection<Guitar>();
+        ObservableCollection<Guitar> availableGuitars = new ObservableCollection<Guitar>();
 
         public MainWindow()
         {
@@ -38,25 +37,78 @@ namespace CA3_s00220273
             var query = from g in db.Guitars
                         select g.StringSize;
 
-            dropdownBrand.ItemsSource = query.ToList();
-        }
-
-
-        //feed StringSize dropdown with guitar colors
-        private void PopulateDropdown()
-        {
-            //var query = Guitar
+            dropdownStringSize.ItemsSource = query.ToList();
+            listBoxAvailable.ItemsSource = availableGuitars;
         }
 
 
 
-        //store selected StringSize from dropdown
-        private void dropdownColor_SelectionChanged(object sender, SelectionChangedEventArgs e)
+
+
+        //store selected StringSize from dropdown and enable search button if other inputs are filled
+        private void dropdownStringSize_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             var query = from g in db.Guitars
                         select g.StringSize;
 
-            
+            if (AreAllInputsFilled() && IsStartDateEarlierThanEndDate())
+            {
+                buttonSearch.IsEnabled = true;
+            }
+
+            else
+            {
+                buttonSearch.IsEnabled = false;
+            }
+
+        }
+
+        //every time a start date is selected, check if the other inputs are filled to enable search button
+        private void datePickerStart_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (IsStartDateEarlierThanEndDate())
+            {
+                textblockDateError.Visibility = Visibility.Hidden;
+
+                if (AreAllInputsFilled())
+                {
+                    buttonSearch.IsEnabled = true;
+                }
+                
+            }
+
+            else
+            {
+                if (IsEndDateFilled() && IsStartDateEarlierThanEndDate() is false)
+                {
+                    textblockDateError.Visibility = Visibility.Visible;
+                }
+                buttonSearch.IsEnabled = false;
+   
+            }
+        }
+
+        //every time an end date is selected, check if the other inputs are filled to enable search button
+        private void datePickerEnd_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (IsStartDateEarlierThanEndDate())
+            {
+                textblockDateError.Visibility = Visibility.Hidden;
+
+                if (AreAllInputsFilled())
+                {
+                    buttonSearch.IsEnabled = true;
+                }
+            }
+
+            else
+            {
+                if (IsStartDateFilled() && IsStartDateEarlierThanEndDate() is false)
+                {
+                    textblockDateError.Visibility = Visibility.Visible;
+                }
+                buttonSearch.IsEnabled = false;
+            }
         }
 
         //store selected start date from date picker
@@ -67,8 +119,9 @@ namespace CA3_s00220273
 
         //feed available listbox with available guitars
 
+
         //guitars tab with all guitar models available
-        private void buttonGetAllGuitars_Click(object sender, RoutedEventArgs e)
+        public void buttonGetAllGuitars_Click(object sender, RoutedEventArgs e)
         {
             var query = from g in db.Guitars
                         select g;
@@ -85,6 +138,105 @@ namespace CA3_s00220273
             datagridBookings.ItemsSource = query.ToList();
         }
 
-   
+        private void buttonSearch_Click(object sender, RoutedEventArgs e)
+        {
+            //var query = from g in db.Guitars
+            //            select g.Brand + " " + g.Model;
+
+            //listBoxAvailable.ItemsSource = query.ToList();
+
+    
+
+        }
+
+
+        //display guitar image when a model is selected in the available listbox
+        private void listBoxAvailable_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        //make the booking if selected guitar is available at that date
+        private void buttonBook_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+        //method to check if dropdown has any value selected
+        public bool IsDropdownSelected()
+        {
+            if (dropdownStringSize.SelectedItem != null)
+            {
+                if (dropdownStringSize.SelectedItem.ToString() == "")
+                {
+                    return false;
+                }
+
+                else
+                {
+                    return true;
+                }
+            }
+
+            else
+            {
+                return false;
+            }
+        }
+
+        //method to check if startDate datepicker has any value selected
+        public bool IsStartDateFilled()
+        {
+            if (datePickerStart.SelectedDate.HasValue)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        //method to check if endDate datepicker has any value selected
+        public bool IsEndDateFilled()
+        {
+            if (datePickerEnd.SelectedDate.HasValue)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool AreAllInputsFilled()
+        {
+            if (IsDropdownSelected() && IsStartDateFilled() && IsEndDateFilled())
+            {
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool IsStartDateEarlierThanEndDate()
+        {
+            if (datePickerStart.SelectedDate < datePickerEnd.SelectedDate)
+            {
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+        }
+
+ 
     }
 }
